22a1c286c4ef539f6e7899e4bcd6e562eef712aa
Nguyễn Hữu Điền <1612104@student.hcmus.edu.vn>
2022-04-26 20:43:12 +0700

    feat: tạo một block mới, được hash bằng SHA256

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('MyCoin');
+const SHA256 = require('crypto-js/sha256');
+
+/**
+ * Cấu trúc của một block.
+ */
+class Block {
+  constructor(index, timestamp, data, previousHash = '') {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+  }
+
+  /**
+   * Tạo hash với SHA256 cho giao dịch.
+   */
+  calculateHash() {
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+  }
+}
