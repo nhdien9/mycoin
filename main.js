c3c67bb5ab8afee293a7327d9755cedaba4b9f15
Nguyễn Hữu Điền <1612104@student.hcmus.edu.vn>
2022-04-26 21:32:13 +0700

    feat: kiểm tra tính hợp lệ của blockchain

--- main.js
+++ main.js
@@ -56,6 +56,26 @@ class Blockchain {
 
     this.chain.push(newBlock);
   }
+
+  /**
+   * Kiểm tra xem một blockchain có hợp lệ hay không?
+   */
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
 
 // Khởi tạo một blockchain mới.
@@ -65,5 +85,16 @@ let mycoin = new Blockchain();
 mycoin.addBlock(new Block(1, "2022-04-25", {amount: 6}));
 mycoin.addBlock(new Block(1, "2022-04-26", {amount: 9}));
 
+// Kiểm tra tính hợp lệ của blockchain đang xét.
+console.log(`Trước khi thay đổi dữ liệu, blockchain có hợp lệ? ${mycoin.isChainValid()}`);
+
 // Xem trạng thái hiện tại của blockchain.
-console.log(JSON.stringify(mycoin, null, 4));
+// console.log(JSON.stringify(mycoin, null, 4));
+
+// Nếu cố ý thay đổi dữ liệu của một block,
+// tính hợp lệ của blockchain sẽ bị phá vỡ!
+mycoin.chain[1].data = { amount: 100 };
+// mycoin.chain[1].hash = mycoin.chain[1].calculateHash();
+
+// Kiểm tra tính hợp lệ của blockchain đang xét.
+console.log(`Sau khi thay đổi dữ liệu, blockchain có hợp lệ? ${mycoin.isChainValid()}`);
