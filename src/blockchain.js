76bec904ba85261023e6cb10563e25d64b9313b2
Nguyễn Hữu Điền <1612104@student.hcmus.edu.vn>
2022-04-30 07:52:48 +0700

    refactor: phân tách xử lý ra file `src/blockchain.js`

--- src/blockchain.js
+++ src/blockchain.js
@@ -146,24 +146,5 @@ class Blockchain {
   }
 }
 
-// Khởi tạo một blockchain mới.
-let mycoin = new Blockchain();
-
-// Test các transaction với 2 tài khoản.
-mycoin.createTransaction(new Transaction('address1', 'address2', 9));
-mycoin.createTransaction(new Transaction('address2', 'address1', 6));
-
-console.log('\nBắt đầu tiến hành quá trình "đào"...');
-mycoin.minePendingTransactions('address3');
-
-// Kiểm tra số dư trong tài khoản "address3",
-// sau khi nó đã "đào" thành công các transaction.
-console.log(`Số dư trong tài khoản "address3" là: ${mycoin.getBalanceOfAddress('address3')}`);
-
-// Tiến hành quá trình "đào" lại một lần nữa!
-console.log('\nBắt đầu tiến hành quá trình "đào" thêm một lần nữa...');
-mycoin.minePendingTransactions('address3');
-
-// Kiểm tra lại số dư trong tài khoản "address3",
-// sau khi nó đã "đào" thành công các transaction.
-console.log(`Số dư trong tài khoản "address3" là: ${mycoin.getBalanceOfAddress('address3')}`);
+module.exports.Blockchain = Blockchain;
+module.exports.Transaction = Transaction;
